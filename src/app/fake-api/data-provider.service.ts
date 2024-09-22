cbeef7baad37cfe800a84c7066d55f3dd2d11631
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2024-09-22 21:25:02 +0200

    add RxState

--- src/app/fake-api/data-provider.service.ts
+++ src/app/fake-api/data-provider.service.ts
@@ -1,34 +1,47 @@
-import { Observable, delay, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { RxState } from '@rx-angular/state';
+import { Observable, delay, map } from 'rxjs';
 import { PeriodicElement } from '../models/periodic-element.model';
 import { ELEMENT_DATA } from '../data/periodic';
-import { Injectable } from '@angular/core';
-
-const currentData: PeriodicElement[] = [...ELEMENT_DATA];
-
-const delayTime = 300 + Math.floor(Math.random() * 1000);
 
 @Injectable({
   providedIn: 'root',
 })
-export class DataProviderService {
-  public getData(filterWord: string): Observable<PeriodicElement[]> {
-    if (filterWord != '') {
-      const filteredData = currentData.filter((element) =>
-        Object.values(element).some((value) =>
-          value.toString().toLowerCase().includes(filterWord)
-        )
-      );
+export class DataProviderService extends RxState<{ data: PeriodicElement[] }> {
+  private delayTime = 300 + Math.floor(Math.random() * 1000);
 
-      return of(filteredData).pipe(delay(delayTime));
-    }
-    return of(currentData).pipe(delay(delayTime));
+  constructor() {
+    super();
+    // Inicjalizujemy stan z ELEMENT_DATA
+    this.set({ data: [...ELEMENT_DATA] });
   }
 
-  public editData(orginal: PeriodicElement, edited: PeriodicElement): void {
-    const index = currentData.indexOf(orginal);
+  // Pobieramy dane ze stanu
+  public getData(filterWord: string): Observable<PeriodicElement[]> {
+    return this.select('data').pipe(
+      delay(this.delayTime),
+      map((data: PeriodicElement[]) =>
+        filterWord
+          ? data.filter((element) =>
+              Object.values(element).some((value) =>
+                value.toString().toLowerCase().includes(filterWord)
+              )
+            )
+          : data
+      )
+    );
+  }
 
-    if (index !== -1) {
-      currentData[index] = edited;
-    }
+  // Edytujemy dane w stanie
+  public editData(original: PeriodicElement, edited: PeriodicElement): void {
+    this.set(({ data }) => {
+      const index = data.findIndex((element) => element === original);
+      if (index !== -1) {
+        const updatedData = [...data];
+        updatedData[index] = edited;
+        return { data: updatedData };
+      }
+      return { data };
+    });
   }
 }
